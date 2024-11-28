7305773a073faefadc7968fb4f208a8d372fd5a7
Saurabh Bhartia <saurabh.bhartiya@juspay.in>
2024-11-28 23:17:22 +0530

    observer design pattern

---
+++ observer/saurabh/DisplayElement.java
@@ -0,0 +1,5 @@
+package observer.saurabh;
+
+public interface DisplayElement {
+    public void display();
+}
---
+++ observer/saurabh/Subject.java
@@ -0,0 +1,9 @@
+package observer.saurabh;
+
+public interface Subject {
+    public void registerObserver(Observer o);
+    public void removeObserver(Observer o);
+    public void notifyObservers();
+}
+// whoever wants to be provider they need to ensure they are
+// implementing above methods
